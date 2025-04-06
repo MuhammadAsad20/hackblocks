015fd48c00e30f8c66476791d5bc610b648ecdc9
MuhammadAsad20 <121595889+MuhammadAsad20@users.noreply.github.com>
2025-04-06 02:39:18 -0700

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -11,9 +11,9 @@ function Features() {
     <div class="flex justify-center flex-wrap gap-x-10 -m-4">
       <div class="p-4 lg:w-1/6 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-       <a className="ml-2 text-blue-500" href="#" target="_blank" rel="noopener noreferrer">
-  <svg fill="currentColor" stroke="none" className="w-6 h-6" viewBox="0 0 24 24">
-    <path d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1s2.48 1.12 2.48 2.5zM0 8.98h5v15H0v-15zm7.5 0h4.78v2.02h.07c.66-1.25 2.28-2.56 4.7-2.56 5.03 0 5.96 3.31 5.96 7.61v8.07h-5v-7.16c0-1.7-.03-3.88-2.37-3.88s-2.74 1.85-2.74 3.76v7.28h-5v-15z" />
+     <a className="ml-2 text-blue-500">
+  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-[25px] h-[40px]" viewBox="0 0 24 24">
+    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
   </svg>
 </a>
           <div class="w-full">
@@ -24,9 +24,9 @@ function Features() {
       </div>
       <div class="p-4 lg:w-1/6 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-      <a className="ml-2 text-pink-500" href="#" target="_blank" rel="noopener noreferrer">
-  <svg fill="currentColor" stroke="none" className="w-6 h-6" viewBox="0 0 24 24">
-    <path d="M7 2C4.239 2 2 4.239 2 7v10c0 2.761 2.239 5 5 5h10c2.761 0 5-2.239 5-5V7c0-2.761-2.239-5-5-5H7zm10 2c1.654 0 3 1.346 3 3v10c0 1.654-1.346 3-3 3H7c-1.654 0-3-1.346-3-3V7c0-1.654 1.346-3 3-3h10zm-5 3a5 5 0 100 10 5 5 0 000-10zm0 2a3 3 0 110 6 3 3 0 010-6zm4.5-2a1 1 0 100 2 1 1 0 000-2z" />
+     <a className="ml-2 text-blue-500">
+  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-[25px] h-[40px]" viewBox="0 0 24 24">
+    <path d="M20.8 4.6c-1.5-1.3-3.7-1.3-5.2 0L12 8l-3.6-3.4c-1.5-1.3-3.7-1.3-5.2 0-1.8 1.6-1.8 4.3 0 5.9L12 21l8.8-10.5c1.7-1.6 1.7-4.3 0-5.9z" />
   </svg>
 </a>
           <div class="w-full">
@@ -37,10 +37,12 @@ function Features() {
       </div>
       <div class="p-4 lg:w-1/6 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-       <a className="ml-2 text-gray-200 hover:text-white" href="#" target="_blank" rel="noopener noreferrer">
-  <svg fill="currentColor" stroke="none" className="w-6 h-6" viewBox="0 0 24 24">
-    <path d="M12 0C5.373 0 0 5.373 0 12a12.04 12.04 0 008.208 11.385c.6.113.82-.26.82-.577v-2.234c-3.338.726-4.042-1.416-4.042-1.416-.546-1.384-1.334-1.753-1.334-1.753-1.09-.745.083-.729.083-.729 1.205.085 1.84 1.236 1.84 1.236 1.07 1.834 2.81 1.304 3.495.997.107-.775.418-1.305.76-1.606-2.665-.304-5.466-1.333-5.466-5.932 0-1.31.469-2.381 1.236-3.22-.124-.303-.535-1.526.117-3.176 0 0 1.008-.322 3.3 1.23a11.51 11.51 0 013.006-.404c1.02.005 2.046.137 3.006.404 2.29-1.552 3.297-1.23 3.297-1.23.654 1.65.243 2.873.12 3.176.77.84 1.235 1.91 1.235 3.22 0 4.61-2.804 5.625-5.476 5.921.43.372.814 1.104.814 2.225v3.293c0 .32.217.694.824.576A12.042 12.042 0 0024 12c0-6.627-5.373-12-12-12z" />\n  </svg>\n</a>
-
+       <a className="ml-2 text-blue-500">
+  <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-[25px] h-[40px]" viewBox="0 0 24 24">
+    <path d="M12 15.5a3.5 3.5 0 100-7 3.5 3.5 0 000 7z" />
+    <path d="M19.4 15a1.65 1.65 0 01.33 1.82l-.03.05a1.65 1.65 0 01-2.33.55 5.44 5.44 0 01-1.63-1.2 5.42 5.42 0 01-1.2-1.63 1.65 1.65 0 01.55-2.33l.05-.03A1.65 1.65 0 0115 8.6a5.42 5.42 0 011.63 1.2 5.44 5.44 0 011.2 1.63 1.65 1.65 0 01.55 2.33z" />
+  </svg>
+</a>
           <div class="w-full">
             <h2 class="title-font font-bold text-2xl text-gray-900">Feature 3</h2>
             <p class="my-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
