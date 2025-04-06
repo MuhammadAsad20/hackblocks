188ed2e2fa4ddf9810f0c9de379835991ce1f3b6
MuhammadAsad20 <121595889+MuhammadAsad20@users.noreply.github.com>
2025-04-06 02:44:46 -0700

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -11,9 +11,9 @@ function Features() {
     <div class="flex justify-center flex-wrap gap-x-10 -m-4">
       <div class="p-4 lg:w-1/6 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-     <a className="ml-2 text-blue-500">
+   <a className="ml-2 text-blue-500">
   <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-[25px] h-[40px]" viewBox="0 0 24 24">
-    <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+    <rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect>
   </svg>
 </a>
           <div class="w-full">
@@ -24,12 +24,12 @@ function Features() {
       </div>
       <div class="p-4 lg:w-1/6 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-     <a className="ml-2 text-blue-500">
+    <a className="ml-2 text-blue-500">
   <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-[25px] h-[40px]" viewBox="0 0 24 24">
     <path d="M20.8 4.6c-1.5-1.3-3.7-1.3-5.2 0L12 8l-3.6-3.4c-1.5-1.3-3.7-1.3-5.2 0-1.8 1.6-1.8 4.3 0 5.9L12 21l8.8-10.5c1.7-1.6 1.7-4.3 0-5.9z" />
   </svg>
 </a>
-          <div class="w-full">
+    <div class="w-full">
             <h2 class="title-font font-bold text-2xl text-gray-900">Feature 2</h2>
             <p class="my-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
           </div>
@@ -37,13 +37,13 @@ function Features() {
       </div>
       <div class="p-4 lg:w-1/6 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
-       <a className="ml-2 text-blue-500">
+     <a className="ml-2 text-blue-500">
   <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" className="w-[25px] h-[40px]" viewBox="0 0 24 24">
-    <path d="M12 15.5a3.5 3.5 0 100-7 3.5 3.5 0 000 7z" />
-    <path d="M19.4 15a1.65 1.65 0 01.33 1.82l-.03.05a1.65 1.65 0 01-2.33.55 5.44 5.44 0 01-1.63-1.2 5.42 5.42 0 01-1.2-1.63 1.65 1.65 0 01.55-2.33l.05-.03A1.65 1.65 0 0115 8.6a5.42 5.42 0 011.63 1.2 5.44 5.44 0 011.2 1.63 1.65 1.65 0 01.55 2.33z" />
+    <circle cx="12" cy="12" r="3"></circle>
+    <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82 2 2 0 0 1-2.82 2.82 1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51v.34a2 2 0 0 1-4 0v-.34a1.65 1.65 0 0 0-1-1.51 1.65 1.65 0 0 0-1.82.33 2 2 0 0 1-2.82-2.82 1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.34a1.65 1.65 0 0 0 1.51-1 1.65 1.65 0 0 0-.33-1.82 2 2 0 0 1 2.82-2.82 1.65 1.65 0 0 0 1.82.33h.01A1.65 1.65 0 0 0 9 4.6V4.25a2 2 0 0 1 4 0v.34a1.65 1.65 0 0 0 1 1.51h.01a1.65 1.65 0 0 0 1.82-.33 2 2 0 0 1 2.82 2.82 1.65 1.65 0 0 0-.33 1.82 1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.34a1.65 1.65 0 0 0-1.51 1z" />
   </svg>
 </a>
-          <div class="w-full">
+  <div class="w-full">
             <h2 class="title-font font-bold text-2xl text-gray-900">Feature 3</h2>
             <p class="my-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
           </div>
